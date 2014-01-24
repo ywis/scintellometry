970fe661aa6994fea4a43479eb36b5653d7ee825
Aaron Berndsen <berndsen@cita.utoronto.ca>
2014-01-24 16:16:13 -0500

    work in progres....

--- scintellometry/folding/correlate.py
+++ scintellometry/folding/correlate.py
@@ -25,8 +25,8 @@ _fref = 150. * u.MHz  # ref. freq. for dispersion measure
 
 
 def correlate(fh1, fh2, dm, nchan, ngate, ntbin, nt, ntint, ntw,
-              dedisperse=None,rfi_filter_raw=None,
-              savefile='test_hdf5',
+              dedisperse=None, rfi_filter_raw=None, fref=_fref,
+              save_xcorr=True, do_foldspec=False,
               t0=None, t1=None, comm=None):
     """
     fh1 : file handle of first data stream
@@ -49,25 +49,20 @@ def correlate(fh1, fh2, dm, nchan, ngate, ntbin, nt, ntint, ntw,
         rank = comm.rank
         size = comm.size
 
-    # output dataset
-    fcorr = h5py.File('savefile.hdf5', 'w') #, driver='mpio', comm=comm)
-
     # initialize the folded spectrum and waterfall
     foldspec = np.zeros((nchan, ngate, ntbin))
     icount = np.zeros((nchan, ngate, ntbin), dtype=np.int64)
     waterfall = np.zeros((nchan, ngate, ntbin))
-    nwsize = nt*ntint//ntw
+    nwsize = nt * ntint // ntw
 
     # find nearest starttime landing on same sample
     if t0 is None:
         t0 = max(fh1.time0, fh2.time0)
         print("Starting at %s" % t0)
     t0 = Time(t0, scale='utc')
-    if t1 is None:
-        pass
     t1 = Time(t1, scale='utc')
 
-    # configure the fh's for xcorr stream
+    # prep the fhs for xcorr stream, setting up channelization, dedispersion...
     for i, fh in enumerate([fh1, fh2]):
         fh.seek(t0)
         fh.dt1 = (1. / fh.samplerate).to(u.s)
@@ -100,104 +95,164 @@ def correlate(fh1, fh2, dm, nchan, ngate, ntbin, nt, ntint, ntw,
             fh.freq = fh.freq[::2]
 
         fh.dt = (dispersion_delay_constant * dm *
-                 ( 1./fh.freq**2 - 1./_fref**2) ).to(u.s).value
+                 ( 1./fh.freq**2 - 1./fref**2) ).to(u.s).value
 
-    # read files in same-size time chunks
-    sr = Fraction(fh1.dtsample.to(u.s).value * fh1.blocksize / fh1.recordsize)
-    sr /= Fraction(fh2.dtsample.to(u.s).value * fh2.blocksize / fh2.recordsize)
-    sr = sr.limit_denominator(5000)
-    n = sr.denominator
-    d = sr.numerator
-    raw1 = fh1.seek_record_read((fh1.this_nskip + rank) * fh1.blocksize,
-                                fh1.blocksize * n)
-    raw2 = fh2.seek_record_read((fh2.this_nskip + rank) * fh2.blocksize,
-                                fh2.blocksize * d)
+        if dedisperse in ['coherent', 'by-channel']:
+            # pre-calculate required turns due to dispersion
+            if fh.nchan > 1:
+                fcoh = (fh.freq[np.newaxis, :] +
+                        fftfreq(ntint, fh.dtsample.value)[:, np.newaxis]
+                        * u.Hz)
+            else:
+                if fh.fedge_at_top:
+                    fcoh = fh.fedge - fh.thisfftfreq(nchan * 2 * ntint,
+                                                     fh.dt1.value) * u.Hz
+                else:
+                    fcoh = fh.fedge + fh.thisfftfreq(nchan * 2 * ntint,
+                                                     fh.dt1.value) * u.Hz
 
-    # time averaging
-    Tavg = Fraction(raw1.shape[0], raw2.shape[0]).limit_denominator(1000)
-    Tden = Tavg.denominator
-    Tnum = Tavg.numerator
+            #set frequency relative to which dispersion is coherently corrected
+            if dedisperse == 'coherent':
+                _fref = fref
+            else:
+                #fref = np.round((fcoh * fh.dtsample).to(1).value)/fh.dtsample
+                _fref = np.repeat(fh.freq.value, ntint) * fh.freq.unit
+            # (check via eq. 5.21 and following in
+            # Lorimer & Kramer, Handbook of Pulsar Astrono
+            dang = (dispersion_delay_constant * dm * fcoh *
+                    (1./_fref-1./fcoh)**2) * 360. * u.deg
 
-    # channel averaging
-    f1 = (fh1.freq.max(), fh1.freq.min(), len(fh1.freq),
-          np.sign(np.diff(fh1.freq).mean()))
-    f2 = (fh2.freq.max(), fh2.freq.min(), len(fh2.freq),
-          np.sign(np.diff(fh2.freq).mean()))
-    f1keep = (fh1.freq > max(f1[1], f2[1])) & (fh1.freq < min(f1[0], f2[0]))
-    f2keep = (fh2.freq > max(f1[1], f2[1])) & (fh2.freq < min(f1[0], f2[0]))
-    np.save('f1freq.npy', fh1.freq.value)
-    np.save('f2freq.npy', fh2.freq.value)
-    Favg = abs(Fraction(np.diff(fh1.freq.value).mean() / np.diff(fh2.freq.value).mean()))
-    Favg = Favg.limit_denominator(5000)
-    Fden = Favg.denominator
-    Fnum = Favg.numerator
-    # print the common freq. grid
-    f1s = fh1.freq[f1keep]
-    f1s = f1s.reshape(f1s.size / Fden, Fden).mean(axis=-1)
-    f2s = fh2.freq[f2keep]
-    f2s = f2s.reshape(f2s.size / Fnum, Fnum).mean(axis=-1)
-    # sort low freq to high freq
-    if f1[3] < 0:
-        f1s = f1s[::-1]
-    if f2[3] < 0:
-        f2s = f2s[::-1]
+            if fh.thisfftfreq is rfftfreq:
+                # order of frequencies is r[0], r[1],i[1],...r[n-1],i[n-1],r[n]
+                # for 0 and n need only real part, but for 1...n-1 need real, imag
+                # so just get shifts for r[1], r[2], ..., r[n-1]
+                dang = dang.to(u.rad).value[1:-1:2]
+            else:
+                dang = dang.to(u.rad).value
 
-    # output dataset
-    # shape is timestep, channel
-    # TODO: save the frequency grids (to figure out future interpolation)
-    fcorr.create_dataset('freq1', data=f1s.value)
-    fcorr.create_dataset('freq2', data=f2s.value)
+            fh.dd_coh = np.exp(dang * 1j).conj().astype(np.complex64)
+    #### done fh setup ###
 
-    # shape is timestep, channel
-    dset = fcorr.create_dataset('corr', (0, f1s.size), dtype='complex64',
-                                maxshape=(None, nchan))
-    # tsteps = fcorr.create_dataset('tsteps', (0), dtype='f', maxshape=None)
+    ## xcorr setup
+    # data-reading params (to help read in same-size time chunks and average
+    # onto the same time-and-frequency grids)
+    (Rf, Tf, NUf, fkeep, freqs, rows) = data_averaging_coeffs(fh1, fh2)
+     #    (nf1, nf2), (Tnum, Tden), (Fnum, Fden), (f1keep, f2keep),
+     #            (freq1, freq2), (raw1_nrows, raw2_nrows)
+     #    (nf1, nf2, Tnum, Tden, Fnum, Fden, f1keep, f2keep,
+     #     freq1, freq2, nrows1, nrows2) = data_averaging_coeffs(fh1, fh2)
+     #    nrows = int(min(nrows1 * Tden / Tnum, nrows2 * Tnum / Tden))
+    nrows = int(min(rows[0] * Tf[1] / Tf[0],
+                    rows[1] * Tf[0] / Tf[1]))
 
+    if save_xcorr:
+        # output dataset
+        outname = "{0}_{1}_{2}_{3}.hdf5".format(fh1.telescope, fh2.telescope,
+                                                t0, t1)
+        fcorr = h5py.File(outname, 'w')  # driver='mpio', comm=comm)
+        ## create the x-corr output file
+        # save the frequency grids to help with future TODO: interpolate onto
+        # same frequency grid. For now the frequencies fall within same bin
+        fcorr.create_dataset('freqs', data=np.hstack([f.to(u.MHz).value
+                                                      for f in freqs]))
+
+        # the x-corr data [tsteps, channels]
+        dset = fcorr.create_dataset('corr', (nrows, freqs[0].size),
+                                    dtype='complex64', maxshape=(None, nchan))
+        #tsteps = fcorr.create_dataset('tsteps', shape=(nrows),
+        #                              dtype='float64', maxshape=(None))
+
+    # start reading the data
+    # this_nskip moves to 't0', rank is for MPI
+    raws = [fh.seek_record_read((fh.this_nskip + rank * Rf[i])
+                               * fh.blocksize, fh.blocksize * Rf[i])
+           for i, fh in enumerate([fh1, fh2])]
     idx = 0
     endread = False
-    while (raw1.size > 0) and (raw2.size > 0):
-        if rfi_filter_raw is not None:
-            raw1, ok = rfi_filter_raw(raw1, nchan)
-            raw2, ok = rfi_filter_raw(raw2, nchan)
+    while np.all([raw.size > 0 for raw in raws]):
+        #raws = [raw1, raw2]
+        vals = raws #[raw1, raw2]
+        chans = [None, None]
+        # prep the data (channelize, dedisperse, ...)
+        for i, fh in enumerate([fh1, fh2]):
+            if rfi_filter_raw is not None:
+                raws[i], ok = rfi_filter_raw(raws[i], nchan)
 
-        if fh1.telescope == 'aro':
-            vals1 = raw1.astype(np.float32)
-        else:
-            vals1 = raw1
-        if fh2.telescope == 'aro':
-            vals2 = raw2.astype(np.float32)
-        else:
-            vals2 = raw2
+            if fh.telescope == 'aro':
+                vals[i] = raws[i].astype(np.float32)
+            else:
+                vals[i] = raws[i]
 
-        if fh1.nchan == 1:
-            # ARO data should fall here
-            chan1 = fh1.thisfft(vals1.reshape(-1, nchan * 2), axis=-1,
-                                overwrite_x=True, **_fftargs)
-        else:  # lofar and gmrt-phased are already channelised
-            chan1 = vals1
-        if fh2.nchan == 1:
-            chan2 = fh2.thisfft(vals2.reshape(-1, nchan * 2), axis=-1,
-                                overwrite_x=True, **_fftargs)
-        else:
-            chan2 = vals2
+            if dedisperse in ['coherent', 'by-channel']:
+                fine = fh.thisfft(vals[i], axis=0, overwrite_x=True,
+                                  **_fftargs)
+                if fh.thisfft is rfft:
+                    fine_cmplx = fine[1:-1].view(np.complex64)
+                    # overwrites parts of fine, as intended
+                    fine_cmplx *= fh.dd_coh
+                else:
+                    fine *= dd_coh
+                vals[i] = fh.thisifft(fine, axis=0, overwrite_x=True,
+                                      **_fftargs)
 
-        # average onto same time grid
-        # this_tsteps = t0 +
-        chan1 = chan1.reshape(Tnum, chan1.shape[0] / Tnum, -1).mean(axis=0)
-        chan2 = chan2.reshape(Tden, chan2.shape[0] / Tden, -1).mean(axis=0)
+            if fh.nchan == 1:
+                # ARO data should fall here
+                chans[i] = fh.thisfft(vals[i].reshape(-1, nchan * 2), axis=-1,
+                                      overwrite_x=True, **_fftargs)
+            else:  # lofar and gmrt-phased are already channelised
+                chans[i] = vals[i]
 
-        # average onto same freq grid
-        chan1 = chan1[..., f1keep]
-        chan2 = chan2[..., f2keep]
-        chan1 = chan1.reshape(-1, chan1.shape[1] / Fden, Fden).mean(axis=-1)
-        chan2 = chan2.reshape(-1, chan2.shape[1] / Fnum, Fnum).mean(axis=-1)
+            # average onto same time grid
+            chans[i] = chans[i].reshape(Tf[i], chans[i].shape[0] / Tf[i], -1)\
+                      .mean(axis=0)
 
-        print("idx", idx, fh1.time(), fh2.time(), dset.shape)
+            # average onto same freq grid
+            chans[i] = chans[i][..., fkeep[i]]
+            chans[i] = chans[i].reshape(-1, chans[i].shape[1] / NUf[i], NUf[i])\
+                                             .mean(axis=-1)
+
+        # current sample positions in stream
+        isr1 = idx * rows[0] + np.arange(rows[0])
+        isr2 = idx * rows[1] + np.arange(rows[1])
+        isr1 = isr1.reshape(-1, Tf[0]).mean(axis=-1)
+        isr2 = isr2.reshape(-2, Tf[1]).mean(axis=-1)
+        tsample1 = (t0 + isr1 * fh1.dtsample)
+        tsample2 = (t0 + isr2 * fh2.dtsample)
         # x-correlate
-        xpower = chan1 * chan2.conjugate()[:, 0:100]
-        curshape = dset.shape
-        dset.resize((curshape[0] + xpower.shape[0], dset.shape[1]))
-        dset[curshape[0]:dset.shape[0]] = xpower
+        xpower = chans[0] * chans[1].conjugate()
+
+        if do_foldspec:
+            # time since start
+#            tsample = np.mean([tsample1.value, tsample2.value], axis=0)
+            # bin in the time series: 0..ntbin-1
+#            ibin = idx * ntbin // nt
+
+            # TODO: dedisperse the individual timeseries
+            for k in xrange(nchan):
+                if dedisperse == 'coherent':
+                    t = tsample  # already dedispersed
+                elif dedisperse in ['incoherent', 'by-channel']:
+                    t = tsample - dt[k]  # dedispersed times
+                elif dedisperse is None:
+                    t = tsample  # do nothing
+                else:
+                    t = tsample - dt[k]
+
+                phase = phasepol(t)  # corresponding PSR phases
+                iphase = np.remainder(phase*ngate,
+                                      ngate).astype(np.int)
+                # sum and count samples by phase bin
+                foldspec[k, :, ibin] += np.bincount(iphase, power[:, k], ngate)
+                icount[k, :, ibin] += np.bincount(iphase, power[:, k] != 0.,
+                                                  ngate)
+
+        if save_xcorr:
+            curshape = dset.shape
+            nx = max(nrows * (idx + rank), curshape[0])
+            dset.resize((nx + nrows, curshape[1]))
+            dset[nrows * (idx + rank): nrows * (idx + rank + 1)] = xpower
+            # tsteps.resize((nx + nrows))
+            # tsteps[nrows * (idx + rank): nrows * (idx + rank + 1)] = tsample1
 
         # read in next dataset if we haven't hit t1 yet
         for fh in [fh1, fh2]:
@@ -206,12 +261,66 @@ def correlate(fh1, fh2, dm, nchan, ngate, ntbin, nt, ntint, ntw,
         if endread:
             break
         else:
-            raw1 = fh1.seek_record_read((fh1.this_nskip + rank + (n * idx))
-                                        * fh1.blocksize, fh1.blocksize * n)
-            raw2 = fh2.seek_record_read((fh2.this_nskip + rank + (d * idx))
-                                        * fh2.blocksize, fh2.blocksize * d)
+            raws = [fh.seek_record_read((fh.this_nskip + (rank + idx) * Rf[i])
+                                        * fh.blocksize, fh.blocksize * Rf[i])
+                    for i, fh in enumerate([fh1, fh2])]
         print("idx",idx, fh1.time(), fh2.time())
-        idx += 1
+        idx += size
 
     fcorr.close()
     return foldspec, icount, waterfall
+
+def data_averaging_coeffs(fh1, fh2):
+    """
+    return the time-and-frequency averaging parameters
+    which help get the data on the same grid(s)
+
+    """
+    ## read the two filestreams in chunks of equal time
+    sr = Fraction(fh1.dtsample.to(u.s).value * fh1.blocksize
+                  / fh1.recordsize)
+    sr /= Fraction(fh2.dtsample.to(u.s).value * fh2.blocksize
+                   / fh2.recordsize)
+    sr = sr.limit_denominator(5000)
+    nf1 = sr.denominator
+    nf2 = sr.numerator
+
+    ## used for re-sizing hdf5 x-corr output
+    raw1_nrows = int(fh1.blocksize * nf1 / fh1.recordsize)
+    raw2_nrows = int(fh2.blocksize * nf2 / fh2.recordsize)
+
+    ## time averaging params
+    Tavg = Fraction(raw1_nrows, raw2_nrows).limit_denominator(1000)
+    Tden = Tavg.denominator
+    Tnum = Tavg.numerator
+
+    ## channel averaging params
+    f1info = (fh1.freq.max(), fh1.freq.min(), len(fh1.freq),
+              np.sign(np.diff(fh1.freq).mean()))
+    f2info = (fh2.freq.max(), fh2.freq.min(), len(fh2.freq),
+              np.sign(np.diff(fh2.freq).mean()))
+    f1keep = (fh1.freq > max(f1info[1], f2info[1])) \
+        & (fh1.freq < min(f1info[0], f2info[0]))
+    f2keep = (fh2.freq > max(f1info[1], f2info[1])) \
+        & (fh2.freq < min(f1info[0], f2info[0]))
+    # np.save('f1freq.npy', fh1.freq.value)
+    # np.save('f2freq.npy', fh2.freq.value)
+    Favg = abs(Fraction(np.diff(fh1.freq.value).mean()
+                        / np.diff(fh2.freq.value).mean()))
+    Favg = Favg.limit_denominator(5000)
+    Fden = Favg.denominator
+    Fnum = Favg.numerator
+    freq1 = fh1.freq[f1keep]
+    freq1 = freq1.reshape(freq1.size / Fden, Fden).mean(axis=-1)
+    freq2 = fh2.freq[f2keep]
+    freq2 = freq2.reshape(freq2.size / Fnum, Fnum).mean(axis=-1)
+    # sort low freq to high freq
+    if f1info[3] < 0:
+        freq1 = freq1[::-1]
+    if f2info[3] < 0:
+        freq2 = freq2[::-1]
+
+    return ((nf1, nf2), (Tnum, Tden), (Fden, Fnum), (f1keep, f2keep),
+            (freq1, freq2), (raw1_nrows, raw2_nrows))
+    #    return (nf1, nf2, Tnum, Tden, Fnum, Fden, f1keep, f2keep, freq1, freq2,
+    #            raw1_nrows, raw2_nrows)
