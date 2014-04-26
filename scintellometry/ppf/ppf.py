0aebe30787aef15c0473b189005e57c88023963a
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2014-04-26 17:01:57 -0400

    Add ppf/__init__.py, different test, more comments in ppf

--- scintellometry/ppf/ppf.py
+++ scintellometry/ppf/ppf.py
@@ -3,6 +3,7 @@
 #  Only import modules that are actually needed and avoid
 #  "from module import *" as much as possible to prevent name clashes.
 import numpy as np
+from astropy.utils.data import get_pkg_data_filename
 
 
 class FastPPF():
@@ -11,7 +12,7 @@ class FastPPF():
     def __init__(self, coefficients_file='Coeffs16384Kaiser-quant.dat'):
         """Constructor"""
         #print "Obtaining Kaiser coefficient from file"
-        self.weights = np.loadtxt(coefficients_file,
+        self.weights = np.loadtxt(get_pkg_data_filename(coefficients_file),
                                   dtype=np.float64).reshape(16,1024)
 
     def __call__(self, data):
@@ -27,7 +28,8 @@ class FastiPPF():
         """Constructor"""
         #print "Obtaining inverse PPF coefficient from file"
         # stored as sample, 16 bins, but want 16 bins, samples
-        self.weights = np.loadtxt(coefficients_file).reshape(-1, 16).T
+        self.weights = (np.loadtxt(get_pkg_data_filename(coefficients_file))
+                        .reshape(-1, 16).T)
 
     def __call__(self, data):
         '''Get the inverse PPF of myinput'''
--- scintellometry/ppf/test_ppf.py
+++ scintellometry/ppf/test_ppf.py
@@ -1,5 +1,8 @@
+from __future__ import division
+
 import numpy as np
 import matplotlib.pylab as plt
+import astropy.units as u
 
 import ppf
 
@@ -7,22 +10,30 @@ if __name__ == '__main__':
     fppf = ppf.FastPPF()
     ippf = ppf.FastiPPF()
     size, shape = fppf.weights.size, fppf.weights.shape
-    t = np.arange(-size, size)
-    timedata = np.sin(t+0.35*np.pi) + np.sin(t*0.33)
-    unfiltered = np.fft.rfft(timedata[size/2:-size/2].reshape(shape) *
+    samplerate = 200*u.MHz
+    t = (np.arange(-size + 0., size) / samplerate).to(u.s)
+    # centre
+    omega = np.array([700.,
+                      800.49,
+                      900.5,
+                      600. + 7./16.,
+                      650. - 6./16.]) / 1024. * u.cycle * samplerate
+    timedata = np.sin(omega[:, np.newaxis] * t).sum(0)
+    unfiltered = np.fft.rfft(timedata[size//2:-size//2].reshape(shape) *
                              fppf.weights.sum(0))
-    filtered = np.array([np.fft.rfft(fppf(timedata[s-size/2:s+size/2]
+    filtered = np.array([np.fft.rfft(fppf(timedata[s-size//2:s+size//2]
                                           .reshape(shape)))
-                                     for s in xrange(size/2,
-                                                     len(timedata)-size/2,
-                                                     shape[-1])])
-    plt.plot(np.abs(unfiltered.T))
-    plt.plot(np.abs(filtered.T))
+                         for s in xrange(size//2,
+                                         len(timedata)-size//2,
+                                         shape[-1])])
+    freq = samplerate - np.fft.rfftfreq(shape[-1], d=1./samplerate)
+    plt.plot(np.arange(1024., 511, -1), np.abs(unfiltered.T))
+    plt.plot(np.arange(1024., 511, -1), np.abs(filtered.T))
 
     inverse = np.fft.irfft(filtered)
     timeback = ippf(inverse)
     plt.clf()
     plt.plot(timeback)
     # plt.plot(inverse.flatten()[size/2-1024:size/2])
-    plt.plot(timedata[size-1024:size])
+    plt.plot(timedata[size-1024:size].value)
     # good to ~0.06/2 = 3%
